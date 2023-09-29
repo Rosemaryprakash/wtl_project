fc2ac2f21a73d4d36f6d4daac687faa3e7f7354b
Rosemary Prakash <roseprakash400@gmail.com>
2023-09-30 01:41:52 +0530

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,69 +66,7 @@
 				</div>
 			</li>
 		</ul>
-	<!-- //main-slider -->
-	<!-- //top-header and slider -->
-	<!-- top-brands -->
 	
-<!-- //top-brands -->
- <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      
-      <!--div class="carousel-inner" role="listbox">
-        <div class="item active">
-         <a href="beverages.html"> <img class="first-slide" src="{{ url('site/images/b1.jpg') }}" alt="First slide"></a>
-       
-        </div>
-        <div class="item">
-         <a href="personalcare.html"> <img class="second-slide " src="{{ url('site/images/b3.jpg') }}" alt="Second slide"></a>
-         
-        </div>
-        <div class="item">
-          <a href="household.html"><img class="third-slide " src="{{ url('site/images/b1.jpg') }}" alt="Third slide"></a>
-          
-        </div>
-      </div-->
-    
-    </div><!-- /.carousel -->	
-<!--banner-bottom-->
-				<!--div class="ban-bottom-w3l">
-					<div class="container">
-					<div class="col-md-6 ban-bottom3">
-							<div class="ban-top">
-								<img src="{{ url('site/images/p2.jpg') }}" class="img-responsive" alt=""/>
-								
-							</div>
-							<div class="ban-img">
-								<div class=" ban-bottom1">
-									<div class="ban-top">
-										<img src="{{ url('site/images/p3.jpg') }}" class="img-responsive" alt=""/>
-										
-									</div>
-								</div>
-								<div class="ban-bottom2">
-									<div class="ban-top">
-										<img src="{{ url('site/images/p4.jpg') }}" class="img-responsive" alt=""/>
-										
-									</div>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-						</div>
-						<div class="col-md-6 ban-bottom">
-							<div class="ban-top">
-								<img src="{{ url('site/images/111.jpg') }}" class="img-responsive" alt=""/>
-								
-								
-							</div>
-						</div>
-						
-						<div class="clearfix"></div>
-					</div>
-				</div-->
-<!--banner-bottom-->
-
 <!-- new -->
 	<div class="newproducts-w3agile">
 		<div class="container">
